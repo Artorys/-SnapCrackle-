f521162b3b11ac69cfb6b380d1fbba02ae40da9b
Artorys <darksoulsgui123@gmail.com>
2022-04-27 11:30:30 -0300

    ready

---
+++ snapCrackle.js
@@ -0,0 +1,30 @@
+function snapCrackle(maxValue)
+{
+    let list = []
+    for(let i = 1;i <= maxValue;i++)
+    {
+        list.push(i)
+    }
+    for(let i = 0;i < list.length;i++)
+    {
+        list[i] = list[i].toString()
+        if(list[i] % 2 != 0 && list[i] % 5 == 0)
+        {
+            list[i] = list[i].replace(list[i]," SnapCrackle, ")
+        }
+        else if(list[i] % 2 != 0)
+        {
+            list[i] = list[i].replace(list[i]," Snap, ")
+        }
+        else if(list[i] % 5 == 0)
+        {
+            list[i] = list[i].replace(list[i]," Crackle, ")
+        }
+        else
+        {
+            list[i] = list[i].replace(list[i], `${list[i]}, `)
+        }
+    }
+    return list.join(" ")
+}
+console.log(snapCrackle(10))
